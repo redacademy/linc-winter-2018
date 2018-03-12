55260442ed750b9dd140156881ad06108affa61b
Calvin Ting <calvinting@protonmail.com>
2018-03-12 15:44:41 -0700

    Removed testing in header.php

--- themes/linc-edge/header.php
+++ themes/linc-edge/header.php
@@ -18,8 +18,7 @@
 
 	<body <?php body_class(); ?>>
 		<div id="page" class="hfeed site">
-		<h1>Testing</h1>
-		<h2>Testing2</h2>
+
 			<a class="skip-link screen-reader-text" href="#content"><?php esc_html( 'Skip to content' ); ?></a>
 
 			<header id="masthead" class="site-header" role="banner">
