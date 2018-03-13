e244611f567d42ecf3aa8b2475607da7d5a70c32
Calvin Ting <calvinting@protonmail.com>
2018-03-13 13:53:15 -0700

    added footer sass style into style.scss and color

--- themes/linc-edge/footer.php
+++ themes/linc-edge/footer.php
@@ -11,7 +11,7 @@
 
 			<footer id="colophon" class="site-footer" role="contentinfo">
 				<div class="site-info">
-					<a href="<?php echo esc_url( 'https://wordpress.org/' ); ?>"><?php printf( esc_html( 'Proudly powered by %s' ), 'WordPress' ); ?></a>
+					<a href="<?php echo esc_url( 'https://wordpress.org/' ); ?>"><?php printf( esc_html( 'LincEdge© 2018. %s' ), 'All Rights Reserved.' ); ?></a>
 				</div><!-- .site-info -->
 			</footer><!-- #colophon -->
 		</div><!-- #page -->
