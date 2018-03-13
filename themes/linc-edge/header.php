85d4b65d51a1e1975b859d3408fdc78063dafc7c
David Yin <davidyin92@hotmail.com>
2018-03-13 13:44:29 -0700

    Edit header CSS.

--- themes/linc-edge/header.php
+++ themes/linc-edge/header.php
@@ -1,39 +1,46 @@
 <?php
 /**
- * The header for our theme.
- *
- * @package linc_edge_Theme
- */
+* The header for our theme.
+*
+* @package linc_edge_Theme
+*/
 
 ?><!DOCTYPE html>
 <html <?php language_attributes(); ?>>
-	<head>
-		<meta charset="<?php bloginfo( 'charset' ); ?>">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<link rel="profile" href="http://gmpg.org/xfn/11">
-		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+    <head>
+        <meta charset="<?php bloginfo( 'charset' ); ?>">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="profile" href="http://gmpg.org/xfn/11">
+        <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
-	<?php wp_head(); ?>
-	</head>
+    <?php wp_head(); ?>
+    </head>
 
-	<body <?php body_class(); ?>>
-		<div id="page" class="hfeed site">
+    <body <?php body_class(); ?>>
+        <div id="page" class="hfeed site">
 
-			<a class="skip-link screen-reader-text" href="#content"><?php esc_html( 'Skip to content' ); ?></a>
+            <a class="skip-link screen-reader-text" href="#content"><?php esc_html( 'Skip to content' ); ?></a>
 
-			<header id="masthead" class="site-header">
-				<div class="masthead-flex">
-					<div class="site-logo">
-						<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
-							<h1 class="site-title">Linc Edge</h1>
-						</a>
-					</div>
+            <header id="masthead" class="site-header">
 
-					<nav id="site-navigation" class="main-navigation">
-						<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
-					</nav><!-- #site-navigation -->
-				</div><!-- .masthead-flex -->
+                    <div class="site-logo">
 
-			</header><!-- #masthead -->
+                        <img class="hamburger-menu" src="<?php echo get_stylesheet_directory_uri(); ?>/images/BurgerMenu-mobile.png">
 
-			<div id="content" class="site-content">
+                        <a href="<?php echo esc_url( home_url( ‘/’ ) ); ?>" rel="home">
+                            <img class="mobile-logo" src="<?php echo get_stylesheet_directory_uri(); ?>/images/LincEdge-Logo-mobile.png">
+                        </a><!-- .mobile-logo -->
+    
+                        <a href="<?php echo esc_url( home_url( ‘/’ ) ); ?>" rel="home">
+                            <img class="desktop-logo" src="<?php echo get_stylesheet_directory_uri(); ?>/images/LincEdge-Logo.svg">
+           				</a><!-- .desktop-logo -->
+           
+                    </div><!-- .site-logo -->
+
+                    <nav id="site-navigation" class="main-navigation">
+                        <?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
+                    </nav><!-- #site-navigation -->
+
+            </header><!-- #masthead -->
+
+            <div id="content" class="site-content">
