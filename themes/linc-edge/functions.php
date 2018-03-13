baf2f98cd01ebf0f2b645f8cf4192be3470189c5
David Yin <davidyin92@hotmail.com>
2018-03-13 14:25:28 -0700

    Create header.js. Enqueue jquery and header.js in functions.php.

--- themes/linc-edge/functions.php
+++ themes/linc-edge/functions.php
@@ -87,6 +87,10 @@ function linc_edge_scripts() {
 
 	wp_enqueue_script( 'red-starter-skip-link-focus-fix', get_template_directory_uri() . '/build/js/skip-link-focus-fix.min.js', array(), '20130115', true );
 
+	wp_enqueue_script('jquery-cdn', 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js', array(), '3.3.1', true);
+
+	wp_enqueue_script( 'header-js', get_template_directory_uri() . '/js/header.js', array('jquery-cdn'), true );
+
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
---
+++ themes/linc-edge/js/header.js
@@ -0,0 +1,8 @@
+(function($) {
+
+    $(document).ready(function () {
+        console.log('js file linked');
+    });
+    
+})( jQuery );
+    
