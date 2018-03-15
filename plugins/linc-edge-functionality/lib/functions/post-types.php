f94fe9e8b0b1783ee9f6840751c5950cd98b795d
David Yin <davidyin92@hotmail.com>
2018-03-14 20:39:29 -0700

    Add custom post type Testimonials and create scss file.

--- plugins/linc-edge-functionality/lib/functions/post-types.php
+++ plugins/linc-edge-functionality/lib/functions/post-types.php
@@ -5,4 +5,58 @@
  * @link  http://codex.wordpress.org/Function_Reference/register_post_type
  */
 
-// Add your custom post types here...
+// Register Custom Post Type Testimonials
+function testimonial_post_type() {
+
+	$labels = array(
+		'name'                  => 'Testimonials',
+		'singular_name'         => 'Testimonial',
+		'menu_name'             => 'Testimonials',
+		'name_admin_bar'        => 'Testimonials',
+		'archives'              => 'Testimonial Archives',
+		'attributes'            => 'Testimonial Attributes',
+		'parent_item_colon'     => 'Parent Item:',
+		'all_items'             => 'All Testimonials',
+		'add_new_item'          => 'Add New Testimonial',
+		'add_new'               => 'Add New',
+		'new_item'              => 'New Testimonial',
+		'edit_item'             => 'Edit Testimonial',
+		'update_item'           => 'Update Testimonial',
+		'view_item'             => 'View Testimonial',
+		'view_items'            => 'View Testimonials',
+		'search_items'          => 'Search Testimonial',
+		'not_found'             => 'Not found',
+		'not_found_in_trash'    => 'Not found in Trash',
+		'featured_image'        => 'Featured Image',
+		'set_featured_image'    => 'Set featured image',
+		'remove_featured_image' => 'Remove featured image',
+		'use_featured_image'    => 'Use as featured image',
+		'insert_into_item'      => 'Insert into Testimonial',
+		'uploaded_to_this_item' => 'Uploaded to this Testimonial',
+		'items_list'            => 'Testimonials list',
+		'items_list_navigation' => 'Testimonials list navigation',
+		'filter_items_list'     => 'Filter Testimonials list',
+	);
+	$args = array(
+		'label'                 => 'Testimonial',
+		'description'           => 'Store Items',
+		'labels'                => $labels,
+		'supports'              => array( 'title', 'editor', 'thumbnail', 'revisions' ),
+		'hierarchical'          => false,
+		'public'                => true,
+		'show_ui'               => true,
+		'show_in_menu'          => true,
+		'menu_position'         => 10,
+		'menu_icon'             => 'dashicons-thumbs-up',
+		'show_in_admin_bar'     => true,
+		'show_in_nav_menus'     => true,
+		'can_export'            => true,
+		'has_archive'           => true,
+		'exclude_from_search'   => false,
+		'publicly_queryable'    => true,
+		'capability_type'       => 'post',
+	);
+	register_post_type( 'testimonial', $args );
+
+}
+add_action( 'init', 'testimonial_post_type', 0 );
--- themes/linc-edge/header.php
+++ themes/linc-edge/header.php
@@ -43,8 +43,6 @@
 
                     <?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
 
-
-
                 </nav><!-- .menu-navigation -->
 
                     
