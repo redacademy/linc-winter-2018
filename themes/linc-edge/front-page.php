006c3b09fa2d2815754b18664b52dbdaf5e61876
Calvin Ting <calvinting@protonmail.com>
2018-03-18 23:05:31 -0700

    top header for mobile front page structred

--- themes/linc-edge/front-page.php
+++ themes/linc-edge/front-page.php
@@ -16,8 +16,8 @@ get_header(); ?>
 		  <div class="banner-container">
 		 	 <div class="banner-text"> <?php echo CFS()->get( 'banner_text' );?> </div>
 			 	<div class="download-container">  
-			  	<div class="download-pics"><?php echo '<img src="' . CFS()->get( 'appleimage' ) .'"/>'; ?></div>
-				<div class="download-pics"><?php echo '<img src="' . CFS()->get( 'googleimage' ) . '"/>'; ?></div>
+				 <div class="download-pics"><?php echo '<img src="' . CFS()->get( 'googleimage' ) . '"/>'; ?></div>  
+				 <div class="download-pics"><?php echo '<img src="' . CFS()->get( 'appleimage' ) .'"/>'; ?></div>
 				</div>
 		  </div>
 		  <div class="theproblem-heading"><?php echo CFS()->get( 'heading' ); ?> </div>
