5614757c2a299e1c677cb014dd2f4c1e50e9e7e7
Calvin Ting <calvinting@protonmail.com>
2018-03-19 22:24:38 -0700

    added flickity and carousel to about page with title for mobile

--- themes/linc-edge/about.php
+++ themes/linc-edge/about.php
@@ -41,7 +41,7 @@ get_header(); ?>
 
 	
 	<div class="customer-container">
-			<div class="customer-header">	<?php echo CFS()->get( 'customer-header' ); ?></div>
+			<div class="customer-header">	<?php echo CFS()->get( 'customer_header' ); ?></div>
 				<div class="carousel"  data-flickity='{ "cellAlign": "left", "contain": true, "wrapAround": true }'>
 			<?php $customers = CFS()->get('customer_logo');
 			foreach ($customers as $customer ): ?>
