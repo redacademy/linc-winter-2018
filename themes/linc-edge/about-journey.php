84df332b589cd22e29e9766afa47f0c68302a1d4
David Yin <davidyin92@hotmail.com>
2018-03-20 14:42:15 -0700

    Add testimonial CSS. Temporary display in about-journey.

--- themes/linc-edge/about-journey.php
+++ themes/linc-edge/about-journey.php
@@ -14,7 +14,30 @@ get_header(); ?>
                 </header>
             <?php endwhile; // End of the loop. ?>
             
+    <?php
+    $args = array(
+        'post_type' => 'testimonial',
+        'posts_per_page' => 1,
+        'title' => 'Chad Myers'
+                );
+    $get_testimonial = get_posts( $args );
+    ?>
+    <?php 
+        foreach ( $get_testimonial as $post ): 
+        setup_postdata($post);
+    ?>
+        <div class="testimonial">
+            <div class="testimonial-inner">
+                <img src="<?php echo CFS() -> get ('image'); ?>">
+                <div class="endorsement">
+                    <p><?php echo CFS() -> get ('endorsers_testimonial'); ?></p>
+                    <h2><?php echo CFS() -> get ('endorsers_name'); ?></h2>
+                </div>
+            </div>
+        </div>
+    <?php endforeach; ?>
 
+    
 
 
 
