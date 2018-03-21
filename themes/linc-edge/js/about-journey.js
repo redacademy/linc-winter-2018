a91c6db4d03ad2c4270a2ce898c3fdc43a2b9573
David Yin <davidyin92@hotmail.com>
2018-03-21 16:05:00 -0700

    Add JS fade  appear about-journey timeline entries on scroll.

---
+++ themes/linc-edge/js/about-journey.js
@@ -0,0 +1,31 @@
+(function($) {
+    $(document).ready(function () {
+        console.log('about-journey linked');
+        /* Check the location of each element */
+        $('.timeline-item').each( function(){
+        
+            var bottom_of_object= $(this).offset().top + $(this).outerHeight();
+            var bottom_of_window = $(window).height();
+            
+            if( bottom_of_object > bottom_of_window){
+            $(this).addClass('timeline-item-hidden');
+            }
+        });
+        
+        
+        $(window).scroll( function(){
+            /* Check the location of each element hidden */
+            $('.timeline-item-hidden').each( function(){
+                
+                var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+                var bottom_of_window = $(window).scrollTop() + $(window).height();
+                
+                /* If the object is completely visible in the window, fadeIn it */
+                if( bottom_of_window > bottom_of_object ){
+                    $(this).animate({'opacity':'1'},700);
+                }
+            });
+        });
+
+    });
+})( jQuery );
