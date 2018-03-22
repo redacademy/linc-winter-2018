28bad9a4b79321523f4af99ab5f404691a24d2fb
David Yin <davidyin92@hotmail.com>
2018-03-21 17:14:16 -0700

    Start work on applying globals, polishing code.

--- themes/linc-edge/about.php
+++ themes/linc-edge/about.php
@@ -10,7 +10,19 @@ get_header(); ?>
 
 			<?php while ( have_posts() ) : the_post(); ?>
 
-				<?php get_template_part( 'template-parts/content', 'page' ); ?>
+        <header class="entry-header">
+          <?php the_title( '<h1 class="entry-title"><span class="title-underline">', '</span></h1>' ); ?>
+        </header><!-- .entry-header -->
+
+        <div class="entry-content">
+          <?php the_content(); ?>
+          <?php
+            wp_link_pages( array(
+              'before' => '<div class="page-links">' . esc_html( 'Pages:' ),
+              'after'  => '</div>',
+            ) );
+          ?>
+        </div><!-- .entry-content -->
 
 			<?php endwhile; ?>
 
