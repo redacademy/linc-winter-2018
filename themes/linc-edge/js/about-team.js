7297dc263e3ce17490dff2fc974af2c33dab909d
David Yin <davidyin92@hotmail.com>
2018-03-23 09:58:38 -0700

    Refactor about-team.php and bio CSS, JS.

--- themes/linc-edge/js/about-team.js
+++ themes/linc-edge/js/about-team.js
@@ -1,74 +1,85 @@
 (function($) {
 
     $(document).ready(function () {
+        // ----------------------- MOBILE -----------------------
+        // Hide all bio paragraphs
+        $('.member-profile-inner .bio-excerpt-mobile p').hide();
+        // Show/hide full bio mobile
+        $('.arrow-show-less-mobile').hide();
+        $('.arrow-show-more-mobile').on('click', function(event) {
+            // Show full bio
+            $(this).parent().siblings('.bio-excerpt-mobile').children("p").show({duration:300});
 
-
-
-            // Hide all bio paragraphs except first paragraph
-            $('.member-profile-inner .bio-excerpt p:not(:first-child)').hide();
-            // Trim first paragraph
-            var CEO_bio_excerpt = $('.member-profile-inner .bio-excerpt:first p');
-            var bio_excerpt = $('.member-profile-inner .bio-excerpt:not(:first) p');
-
-            var minimize_char_count_CEO = 80;   
-            var minimize_char_count = 45;
-
-            bio_excerpt.each(function(){    
-                var text = $(this).text();        
-                if(text.length > minimize_char_count ) {
-                    $(this).html(text.slice(0,minimize_char_count )+ '<span>... </span>'
-                    + '<span style="display:none;">'
-                    + text.slice(minimize_char_count , text.length)
-                    + '</span>'
-                    );
-                }
-            });
+            $(this).hide();
+            $(this).siblings('.arrow-show-less-mobile').show();
+        })
+        $('.arrow-show-less-mobile').on('click', function(event) {
+            // Hide full bio
+            $(this).parent().siblings('.bio-excerpt-mobile').children("p").hide({duration:300});
             
-            CEO_bio_excerpt.each(function(){    
-                var CEO_text = $(this).text();        
-                if(CEO_text.length > minimize_char_count ) {
-                    $(this).html(CEO_text.slice(0,minimize_char_count_CEO )+ '<span>... </span>'
-                    + '<span style="display:none;">'
-                    + CEO_text.slice(minimize_char_count_CEO , CEO_text.length)
-                    + '</span>'
-                    );
-                }
-            });
+            $(this).hide();
+            $(this).siblings('.arrow-show-more-mobile').show();
+        })
 
-            // Show/hide full bio
-            $('.arrow-show-less').hide();
-            $('.arrow-show-more').on('click', function(event) {
-                console.log('show bio');
-                // Hide ellipses
-                $(this).parent().siblings('.bio-excerpt').children().children().hide();
-                // Show full bio
-                $(this).parent().siblings('.bio-excerpt').children().siblings().show();
-                $(this).parent().siblings('.bio-excerpt').children().children().next().show();
-                // Keep headshot from moving (too much)
-                $(this).parent().parent().siblings('.headshot').css({"align-self":"flex-start", "margin-top":"0.65rem"});
+        // ----------------------- DESKTOP -----------------------
+        // Hide all bio paragraphs except first paragraph
+        $('.member-profile-inner .bio-excerpt-desktop p:not(:first-child)').hide();
+        // Trim first paragraph
+        var bio_excerpt = $('.member-profile-inner .bio-excerpt-desktop:not(:first) p');
+        var CEO_bio_excerpt = $('.member-profile-inner .bio-excerpt-desktop:first p');
 
-                $(this).hide();
-                $(this).siblings('.arrow-show-less').show();
-            })
-            $('.arrow-show-less').on('click', function(event) {
-                // Show ellipses
-                $(this).parent().siblings('.bio-excerpt').children().children().show();
-                // Hide full bio
-                $(this).parent().siblings('.bio-excerpt').children().children().next().hide();
-                $('.member-profile-inner .bio-excerpt p:not(:first-child)').hide();
-                // Place headshot back
-                $(this).parent().parent().siblings('.headshot').css({"align-self":"center", "margin-top":"0"});
-                
-                $(this).hide();
-                $(this).siblings('.arrow-show-more').show();
-            })
+        var minimize_char_count = 45;
+        var minimize_char_count_CEO = 80;   
 
-            // $(window).on('resize', function () { 
-            //     if (document.body.clientWidth < 650) {
+        bio_excerpt.each(function(){    
+            var text = $(this).text();        
+            if(text.length > minimize_char_count ) {
+                $(this).html(text.slice(0,minimize_char_count )+ '<span class="elip">... </span>'
+                + '<span class="trimmed-text" style="display:none;">'
+                + text.slice(minimize_char_count , text.length)
+                + '</span>'
+                );
+            }
+        });
+        
+        CEO_bio_excerpt.each(function(){    
+            var CEO_text = $(this).text();        
+            if(CEO_text.length > minimize_char_count ) {
+                $(this).html(CEO_text.slice(0,minimize_char_count_CEO )+ '<span class="elip">... </span>'
+                + '<span class="trimmed-text" style="display:none;">'
+                + CEO_text.slice(minimize_char_count_CEO , CEO_text.length)
+                + '</span>'
+                );
+            }
+        });
 
-            //     }
-            //  });
-        // } // end window on load
+        // Show/hide full bio desktop
+        $('.arrow-show-less-desktop').hide();
+        $('.arrow-show-more-desktop').on('click', function(event) {
+            // Hide ellipses
+            $(this).parent().siblings('.bio-excerpt-desktop').children("p").children(".elip").hide();
+            // Show full bio
+            $(this).parent().siblings('.bio-excerpt-desktop').children("p").show();
+            $(this).parent().siblings('.bio-excerpt-desktop').children("p").children(".trimmed-text").show();
+            // Keep headshot from moving (too much)
+            // $(this).parent().parent().siblings('.headshot').css({"align-self":"flex-start", "margin-top":"0.6rem"});
+
+            $(this).hide();
+            $(this).siblings('.arrow-show-less-desktop').show();
+        })
+        $('.arrow-show-less-desktop').on('click', function(event) {
+            // Show ellipses
+            $(this).parent().siblings('.bio-excerpt-desktop').children("p").children(".elip").show();
+            // Hide full bio
+            $(this).parent().siblings('.bio-excerpt-desktop').children("p").children(".trimmed-text").hide();
+            $(this).parent().siblings('.bio-excerpt-desktop').children("p:not(:first-child)").hide();
+            // Place headshot back
+            $(this).parent().parent().siblings('.headshot').css({"align-self":"center", "margin-top":"0"});
+            
+            $(this).hide();
+            $(this).siblings('.arrow-show-more-desktop').show();
+        })
+
     });
     
 })( jQuery );
