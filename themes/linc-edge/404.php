898c2916a4ed44ca339b21baa1d943ad8f4e2fbf
Ryan Cs. Ku <ryancsku@gmail.com>
2018-03-23 17:30:08 -0700

    Incorporate and style "error 404" page into project files in PHP and SCSS format.

--- themes/linc-edge/404.php
+++ themes/linc-edge/404.php
@@ -12,41 +12,20 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
-			<section class="error-404 not-found">
-				<header class="page-header">
-					<h1 class="page-title"><?php echo esc_html( 'Oops! That page can&rsquo;t be found.' ); ?></h1>
-				</header><!-- .page-header -->
+			<section class="error-404">
 
-				<div class="page-content">
-					<p><?php echo esc_html( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?' ); ?></p>
+				<header class="entry-header">
+					<h1 class="error-title entry-title"><span class="title-underline"><?php echo esc_html( 'Error 404' ); ?></span></h1>
+				</header><!-- .error-header -->
 
-					<?php get_search_form(); ?>
+				<div class="error-content">
+					<p class="error-description"><?php echo esc_html( 'This page could not be found.' ); ?></p>
+				</div><!-- .error-content -->
 
-					<?php the_widget( 'WP_Widget_Recent_Posts' ); ?>
+        <div class="error-button">
+          <button><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">Back to Home</a></button>
+        <div class="error-button">
 
-					<?php if ( linc_edge_categorized_blog() ) : // Only show the widget if site has multiple categories. ?>
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php echo esc_html( 'Most Used Categories' ); ?></h2>
-						<ul>
-						<?php
-							wp_list_categories( array(
-								'orderby'    => 'count',
-								'order'      => 'DESC',
-								'show_count' => 1,
-								'title_li'   => '',
-								'number'     => 10,
-							) );
-						?>
-						</ul>
-					</div><!-- .widget -->
-					<?php endif; ?>
-
-					<?php
-						$archive_content = '<p>' . sprintf( esc_html( 'Try looking in the monthly archives. %1$s' ), convert_smilies( ':)' ) ) . '</p>';
-						the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
-					?>
-
-				</div><!-- .page-content -->
 			</section><!-- .error-404 -->
 
 		</main><!-- #main -->
