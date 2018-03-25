e8a53bee8e87f7fc3544f8af18f182f66a7f0e32
Calvin Ting <calvinting@protonmail.com>
2018-03-24 20:17:51 -0700

    added excerpt function for mobile and desktop, added styling for desktop

--- themes/linc-edge/functions.php
+++ themes/linc-edge/functions.php
@@ -123,20 +123,44 @@ add_action( 'wp_enqueue_scripts', 'linc_edge_scripts' );
 
 
 
-function et_excerpt_length($length) {
-	return 10;
+
+
+
+
+
+
+
+function excerpt($limit) {
+	$excerpt = explode(' ', get_the_excerpt(), $limit);
+
+	if (count($excerpt) >= $limit) {
+		array_pop($excerpt);
+		$excerpt = implode(" ", $excerpt) . '...';
+	} else {
+		$excerpt = implode(" ", $excerpt);
+	}
+
+	$excerpt = preg_replace('`\[[^\]]*\]`', '', $excerpt);
+
+	return $excerpt;
 }
-add_filter('excerpt_length', 'et_excerpt_length');
 
+function content($limit) {
+  $content = explode(' ', get_the_content(), $limit);
 
+  if (count($content) >= $limit) {
+	  array_pop($content);
+	  $content = implode(" ", $content) . '...';
+  } else {
+	  $content = implode(" ", $content);
+  }
 
+  $content = preg_replace('/\[.+\]/','', $content);
+  $content = apply_filters('the_content', $content); 
+  $content = str_replace(']]>', ']]&gt;', $content);
 
-function new_excerpt_more( $more ) {
-    return '...';
+  return $content;
 }
-add_filter('excerpt_more', 'new_excerpt_more');
-
-
 
 
 
