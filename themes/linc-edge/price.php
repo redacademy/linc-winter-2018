bd2687a448d509f959b288ca7b70f68622975185
David Yin <davidyin92@hotmail.com>
2018-03-25 15:11:43 -0700

    Tidy and refactor price.php and CSS.

--- themes/linc-edge/price.php
+++ themes/linc-edge/price.php
@@ -9,15 +9,11 @@ get_header(); ?>
 <div id="primary" class="content-area">
   <main id="main" class="site-main" role="main">
 
-    <?php while ( have_posts() ) : the_post(); ?>
-
       <header class="entry-header">
         <?php esc_html(the_title( '<h1 class="entry-title"><span class="title-underline">', '</span></h1>' )); ?>
       </header><!-- .entry-header -->
 
-    <?php endwhile; ?><!-- ending while loop -->
-
-    <div class="pricing-plan-container">
+    <div class="pricing-plans-container">
       <?php $fields = CFS()->get(esc_html( 'pricing_plan' )); foreach ( $fields as $field ): ?>
 
         <div class="pricing-plan-content-container">
@@ -29,31 +25,27 @@ get_header(); ?>
         </div><!-- .pricing-plan-content-container -->
         
       <?php endforeach; ?><!-- ending foreach loop -->
-    </div><!-- .pricing-plan-container -->
+    </div><!-- .pricing-plans-container -->
 
-    <div class="subscription-description-container">
-      <?php $subscribes = CFS()->get( esc_html('subscribe_description_content') ); foreach ( $subscribes as $subscribe ): ?>
+    <?php $subscribes = CFS()->get( esc_html('subscribe_description_content') ); foreach ( $subscribes as $subscribe ): ?>
 
-        <div class="subscription-description-content-container">
-          <p class="subscription-description"><?php echo esc_html($subscribe['subscribe_description']); ?></p><!-- .subscription-description -->
-        </div><!-- .subscription-description-content-container -->
+      <div class="subscription-description-container">
+        <p class="subscription-description"><?php echo esc_html($subscribe['subscribe_description']); ?></p><!-- .subscription-description -->
+      </div><!-- .subscription-description-container -->
 
-      <?php endforeach; ?><!-- ending foreach loop -->
-    </div><!-- .subscription-description-container -->
+    <?php endforeach; ?><!-- ending foreach loop -->
 
     <div class="subscription-form-container">
       <?php echo CFS()->get( esc_html('subscribe_form') ); ?>
     </div><!-- .subscription-form-container -->
 
-    <div class="subscription-condition-container">
-      <?php $conditions = CFS()->get( esc_html('subscribe_condition_content') ); foreach ( $conditions as $condition ): ?>
+    <?php $conditions = CFS()->get( esc_html('subscribe_condition_content') ); foreach ( $conditions as $condition ): ?>
 
-        <div class="subscription-condition-content-container">
-          <p class="subscription-condition"><?php echo esc_html($condition['subscribe_condition']); ?></p><!-- .subscription-condition -->
-        </div><!-- subscription-condition-content-container -->
-    
-      <?php endforeach; ?><!-- ending foreach loop -->
-    </div><!-- .subscription-condition-container -->
+      <div class="subscription-condition-container">
+        <p class="subscription-condition"><?php echo esc_html($condition['subscribe_condition']); ?></p><!-- .subscription-condition -->
+      </div><!-- subscription-condition-container -->
+  
+    <?php endforeach; ?><!-- ending foreach loop -->
           
   </main><!-- .site-main -->
 </div><!-- .content-area -->
