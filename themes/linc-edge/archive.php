4a6c1e62581e3f80572517db029254cc8e49d278
David Yin <davidyin92@hotmail.com>
2018-03-26 15:54:10 -0700

    Refactor back-to-top-btnn and JS smooth scroll.

--- themes/linc-edge/archive.php
+++ themes/linc-edge/archive.php
@@ -40,7 +40,7 @@ get_header(); ?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
-	<a href="#page" id="back-to-top-btn" title="Go to top"><i class="fas fa-arrow-up"></i></a>
+	<a id="back-to-top-btn" title="Go to top"><i class="fas fa-arrow-up"></i></a>
 
 	<div class="backhome-container">
 
