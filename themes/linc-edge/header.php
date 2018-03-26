a79638485ec621255763abadc6ca6a3d55452a08
Ryan Cs. Ku <ryancsku@gmail.com>
2018-03-26 15:55:56 -0700

    Reformart "header.php" file.

--- themes/linc-edge/header.php
+++ themes/linc-edge/header.php
@@ -7,45 +7,42 @@
 
 ?><!DOCTYPE html>
 <html <?php language_attributes(); ?>>
-    <head>
-        <meta charset="<?php bloginfo( 'charset' ); ?>">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="profile" href="http://gmpg.org/xfn/11">
-        <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
-
+  <head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="profile" href="http://gmpg.org/xfn/11">
+    <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
     <?php wp_head(); ?>
-    </head>
+  </head>
 
-    <body <?php body_class(); ?>>
-        <div id="page" class="hfeed site" title="LincEdge">
+  <body <?php body_class(); ?>>
+    <div id="page" class="hfeed site" title="LincEdge">
 
-            <a class="skip-link screen-reader-text" href="#content"><?php esc_html( 'Skip to content' ); ?></a>
+      <a class="skip-link screen-reader-text" href="#content"><?php esc_html( 'Skip to content' ); ?></a>
 
-            <header id="masthead" class="site-header">
+      <header id="masthead" class="site-header">
 
-                <nav class="menu-navigation">
-                    
-                    <button class="hamburger hamburger--squeeze" type="button">
-                        <span class="hamburger-box">
-                            <span class="hamburger-inner"></span>
-                        </span>
-                    </button>
+        <nav class="menu-navigation">
+            
+          <button class="hamburger hamburger--squeeze" type="button">
+            <span class="hamburger-box">
+                <span class="hamburger-inner"></span>
+            </span>
+          </button>
 
-                    <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
-                        <img class="mobile-logo" alt="LincEdge Mobile" src="<?php echo get_stylesheet_directory_uri(); ?>/images/LincEdge-Logo-mobile.png">
-                    </a>
+          <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+            <img class="mobile-logo" alt="LincEdge Mobile" src="<?php echo get_stylesheet_directory_uri(); ?>/images/LincEdge-Logo-mobile.png">
+          </a>
 
-                    <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
-                        <img class="desktop-logo" alt="LincEdge Desktop" src="<?php echo get_stylesheet_directory_uri(); ?>/images/LincEdge-Logo-Copyright.png">
-                    </a>
+          <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+            <img class="desktop-logo" alt="LincEdge Desktop" src="<?php echo get_stylesheet_directory_uri(); ?>/images/LincEdge-Logo-Copyright.png">
+          </a>
 
-       
-   
-                    <?php wp_nav_menu( array( esc_html('theme_location') => esc_html('primary'), esc_html('menu_id') => esc_html('primary-menu') ) ); ?>
-                 
-                </nav><!-- .menu-navigation -->
+          <?php wp_nav_menu( array( esc_html('theme_location') => esc_html('primary'), esc_html('menu_id') => esc_html('primary-menu') ) ); ?>
+        
+        </nav><!-- .menu-navigation -->
 
-            </header><!-- #masthead -->
+      </header><!-- #masthead -->
 
-            <div id="content" class="site-content">
+    <div id="content" class="site-content">
