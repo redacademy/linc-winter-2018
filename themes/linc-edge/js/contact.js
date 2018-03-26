ae28edbe94693d34f02c8676e527fd111584d13c
Calvin Ting <calvinting@protonmail.com>
2018-03-26 10:27:03 -0700

    deleted top function (not used)

--- themes/linc-edge/js/contact.js
+++ themes/linc-edge/js/contact.js
@@ -15,18 +15,3 @@ $(document).ready(function(){
 
 
 })( jQuery );
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        document.getElementById("back-to-top-btn").style.display = "block";
-    } else {
-        document.getElementById("back-to-top-btn").style.display = "none";
-    }
-}
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-}
