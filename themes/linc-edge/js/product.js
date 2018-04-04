a2a569fe68e6443f20d6b66bd7d1756b9d478217
David Yin <davidyin92@hotmail.com>
2018-04-04 12:15:31 -0700

    Incorporate Flickity on HIW 2nd row. Fix reuse of '.carousel-2' class with Products.

--- themes/linc-edge/js/product.js
+++ themes/linc-edge/js/product.js
@@ -1,84 +1,88 @@
 jQuery(document).ready(function($){
-                // ADD COMMENTS
-                if($('.carousel').length){
-                    $('.carousel').flickity({
-                        // options
-                        cellAlign: 'left',
-                        contain: true
-                      });
-                }
-        
-                $("#button-feature").click(function(){
-                    $(".features-content-container").css("display", "block");
-                    $(".benefits-content-container").css("display", "none");
-        
-                });
-        
-                $("#button-benefit").click(function(){
-                    if ($(window).width()<=650 ){
-                    $("#benefit-mobile").css("display","block");
-                    $("#benefit-desktop").css("display", "none");
-                    $(".features-content-container").css("display", "none");
-                    $('.carousel-2').flickity({
-                        cellAlign: 'left',
-                        contain: true
-                      });
-                    } else{
-                    $("#benefit-mobile").css("display","none");
-                    $("#benefit-desktop").css("display", "block");
-                    $(".features-content-container").css("display", "none");
-                    }
-                });
-                $(".button-feature").addClass("button-active");
-        
-                $("#button-benefit").click(function(){
-                $(".button-benefit").addClass("button-active");
-                $(".button-feature").removeClass("button-active");
-              });    
-                $("#button-feature").click(function(){
-                $(".button-feature").addClass("button-active");
-                $(".button-benefit").removeClass("button-active");
-              });
-
-            $(window).on('resize', function () {
-                if (/Android|webOS|iPhone|iPod|BlackBerry/i.test(navigator.userAgent)){
-                    // do nothing ?
-                }
-                else if (document.body.clientWidth > 650) {
-                    $('.features-content-container').css('display','block');
-                    $('#benefit-desktop').css('display','flex');
-                    $('#benefit-mobile').css('display','none');
-                }
-            });
-            $(window).on('resize', function () {
-                if (/Android|webOS|iPhone|iPod|BlackBerry/i.test(navigator.userAgent)){
-                    // do nothing ?
-                }
-                else if (document.body.clientWidth < 650) {
-                    $('.features-content-container').css('display','block');
-                    $('#benefit-desktop').css('display','none');
-                }
-            });
-            // $("#menu-item-158").click(function(){
-            //     if ($(window).width()<=650 ){
-            //         $("#benefit-mobile").css("display","block");
-            //         $("#benefit-desktop").css("display", "none");
-            //         $(".features-content-container").css("display", "none");
-            //         $('.carousel-2').flickity({
-            //             cellAlign: 'left',
-            //             contain: true
-            //           });
-            //           $(".button-benefit").addClass("button-active");
-            //           $(".button-feature").removeClass("button-active");
-            //         } else{
-            //         $("#benefit-mobile").css("display","none");
-            //         $("#benefit-desktop").css("display", "block");
-            //         $(".features-content-container").css("display", "none");
-            //         }
-            //     });
-
-        
-
-                
-      
+    // ADD COMMENTS
+    if($('.carousel').length){
+        $('.carousel').flickity({
+            // options
+            cellAlign: 'left',
+            contain: true
         });
+    }
+
+    if($('.carousel-2').length){
+        $('.carousel-2').flickity({
+            // options
+            cellAlign: 'left',
+            contain: true
+        });
+    }
+
+    $("#button-feature").click(function(){
+        $(".features-content-container").css("display", "block");
+        $(".benefits-content-container").css("display", "none");
+
+    });
+
+    $("#button-benefit").click(function(){
+        if ($(window).width()<=650 ){
+            $("#benefit-mobile").css("display","block");
+            $("#benefit-desktop").css("display", "none");
+            $(".features-content-container").css("display", "none");
+            $('.carousel-mobile').flickity({
+                cellAlign: 'left',
+                contain: true
+            });
+        } else{
+            $("#benefit-mobile").css("display","none");
+            $("#benefit-desktop").css("display", "block");
+            $(".features-content-container").css("display", "none");
+        }
+    });
+    $(".button-feature").addClass("button-active");
+
+    $("#button-benefit").click(function(){
+        $(".button-benefit").addClass("button-active");
+        $(".button-feature").removeClass("button-active");
+    });    
+    $("#button-feature").click(function(){
+        $(".button-feature").addClass("button-active");
+        $(".button-benefit").removeClass("button-active");
+    });
+
+    $(window).on('resize', function () {
+        if (/Android|webOS|iPhone|iPod|BlackBerry/i.test(navigator.userAgent)){
+            // do nothing ?
+        }
+        else if (document.body.clientWidth > 650) {
+            $('.features-content-container').css('display','block');
+            $('#benefit-desktop').css('display','flex');
+            $('#benefit-mobile').css('display','none');
+        }
+    });
+    $(window).on('resize', function () {
+        if (/Android|webOS|iPhone|iPod|BlackBerry/i.test(navigator.userAgent)){
+            // do nothing ?
+        }
+        else if (document.body.clientWidth < 650) {
+            $('.features-content-container').css('display','block');
+            $('#benefit-desktop').css('display','none');
+        }
+    });
+    // $("#menu-item-158").click(function(){
+    //     if ($(window).width()<=650 ){
+    //         $("#benefit-mobile").css("display","block");
+    //         $("#benefit-desktop").css("display", "none");
+    //         $(".features-content-container").css("display", "none");
+    //         $('.carousel-2').flickity({
+    //             cellAlign: 'left',
+    //             contain: true
+    //           });
+    //           $(".button-benefit").addClass("button-active");
+    //           $(".button-feature").removeClass("button-active");
+    //         } else{
+    //         $("#benefit-mobile").css("display","none");
+    //         $("#benefit-desktop").css("display", "block");
+    //         $(".features-content-container").css("display", "none");
+    //         }
+    //     });
+
+});
